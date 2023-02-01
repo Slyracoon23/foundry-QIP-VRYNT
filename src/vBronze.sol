a0ffbc39dc79325296359d9334778e4bf39ab6bc
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2023-02-01 11:23:36 -0500

    Create vBronze.sol

---
+++ src/vBronze.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./VerificationNFT.sol";
+
+contract vBronze is VerificationNFT{
+
+    constructor() VerificationNFT("VRYNT Bronze", "vBRO") {
+    }
+
+}
